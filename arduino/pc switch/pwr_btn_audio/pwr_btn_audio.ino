29c1a3d53c5c488f08b4ee06083cd64345d7bd9f
Chesters-CZ <bezouskavit@gmail.com>
2022-11-20 19:07:42 +0100

    ještě více startup zvuků, ještě více falunské uzenky

--- arduino/pc switch/pwr_btn_audio/pwr_btn_audio.ino
+++ arduino/pc switch/pwr_btn_audio/pwr_btn_audio.ino
@@ -31,7 +31,7 @@ void loop() {
       tmrpcm.stopPlayback();
       if (SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
         tmrpcm.setVolume(5);
-        switch (round(random(17) - 1)) {
+        switch (round(random(45) - 1)) {
           case 0:
             tmrpcm.play("startup.wav");
             Serial.println(F("Played track 1"));
@@ -99,6 +99,118 @@ void loop() {
           case 16:
             tmrpcm.play("startu17.wav");
             Serial.println(F("Played track 17"));
+            break;
+          case 17:
+            tmrpcm.play("startu18.wav");
+            Serial.println(F("Played track 18"));
+            break;
+          case 18:
+            tmrpcm.play("startu19.wav");
+            Serial.println(F("Played track 19"));
+            break;
+          case 19:
+            tmrpcm.play("startu20.wav");
+            Serial.println(F("Played track 20"));
+            break;
+          case 20:
+            tmrpcm.play("startu21.wav");
+            Serial.println(F("Played track 21"));
+            break;
+          case 21:
+            tmrpcm.play("startu22.wav");
+            Serial.println(F("Played track 22"));
+            break;
+          case 22:
+            tmrpcm.play("startu23.wav");
+            Serial.println(F("Played track 23"));
+            break;
+          case 23:
+            tmrpcm.play("startu24.wav");
+            Serial.println(F("Played track 24"));
+            break;
+          case 24:
+            tmrpcm.play("startu25.wav");
+            Serial.println(F("Played track 25"));
+            break;
+          case 25:
+            tmrpcm.play("startu26.wav");
+            Serial.println(F("Played track 26"));
+            break;
+          case 26:
+            tmrpcm.play("startu27.wav");
+            Serial.println(F("Played track 27"));
+            break;
+          case 27:
+            tmrpcm.play("startu28.wav");
+            Serial.println(F("Played track 28"));
+            break;
+          case 28:
+            tmrpcm.play("startu29.wav");
+            Serial.println(F("Played track 29"));
+            break;
+          case 29:
+            tmrpcm.play("startu30.wav");
+            Serial.println(F("Played track 30"));
+            break;
+          case 30:
+            tmrpcm.play("startu31.wav");
+            Serial.println(F("Played track 31"));
+            break;
+          case 31:
+            tmrpcm.play("startu32.wav");
+            Serial.println(F("Played track 32"));
+            break;
+          case 32:
+            tmrpcm.play("startu33.wav");
+            Serial.println(F("Played track 33"));
+            break;
+          case 33:
+            tmrpcm.play("startu34.wav");
+            Serial.println(F("Played track 34"));
+            break;
+          case 34:
+            tmrpcm.play("startu35.wav");
+            Serial.println(F("Played track 35"));
+            break;
+          case 35:
+            tmrpcm.play("startu36.wav");
+            Serial.println(F("Played track 36"));
+            break;
+          case 36:
+            tmrpcm.play("startu37.wav");
+            Serial.println(F("Played track 37"));
+            break;
+          case 37:
+            tmrpcm.play("startu38.wav");
+            Serial.println(F("Played track 38"));
+            break;
+          case 38:
+            tmrpcm.play("startu39.wav");
+            Serial.println(F("Played track 39"));
+            break;
+          case 39:
+            tmrpcm.play("startu40.wav");
+            Serial.println(F("Played track 40"));
+            break;
+          case 40:
+            tmrpcm.play("startu141.wav");
+            Serial.println(F("Played track 41"));
+            break;
+          case 41:
+            tmrpcm.play("startu42.wav");
+            Serial.println(F("Played track 42"));
+            break;
+          case 42:
+            tmrpcm.play("startu43.wav");
+            Serial.println(F("Played track 43"));
+            break;
+          case 43:
+            tmrpcm.play("startu44.wav");
+            Serial.println(F("Played track 44"));
+            break;
+          case 44:
+            tmrpcm.play("startu45.wav");
+            Serial.println(F("Played track 45"));
             break;
           default:
             tmrpcm.play("startup2.wav");
