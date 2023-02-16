e2cdd1128c8bfe60b5b899734265fea656aea35d
Chesters-CZ <bezouskavit@gmail.com>
2023-02-16 21:05:37 +0100

    automatická detekce počtu souborů & morse code

--- arduino/project fily/pwr_btn_audio/pwr_btn_audio.ino
+++ arduino/project fily/pwr_btn_audio/pwr_btn_audio.ino
@@ -1,3 +1,5 @@
+#include <cww_MorseTx.h>
+
 #define SD_ChipSelectPin 4  //using digital pin 4 on arduino nano 328, can use other pins
 #include <TMRpcm.h>           //  also need to include this library...
 
@@ -5,41 +7,71 @@ TMRpcm tmrpcm;
 
 const int commsOutPin = 7;
 const int commsInPin = 8;
-const int totalTracks = -1;
+int totalTracks = -1;
 byte commsIn = 0;
 
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  Serial.println("Audio start");
+
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-
-  File root = SD.open("/");
-  File currentFile;
-  while (true) {
-    currentFile = root.openNextFile();
-    if (currentFile) {
-      totalTracks++;
-    }
-    else {
-      break;
-    }
-  }
+  delay(1000);
 
   digitalWrite(LED_BUILTIN, LOW);
   delay(250);
-  Serial.begin(9600);
 
   pinMode(commsOutPin, OUTPUT);
   pinMode(commsInPin, INPUT_PULLUP);
+
+  
+
+  if (!SD.begin(SD_ChipSelectPin)){
+        Serial.println("SD could not be initialized.");
+    cww_MorseTx morse(0, 10, 9, 1000);
+    morse.send("NO SD");
+    while (true){
+    }
+  }
+  
+  File root = SD.open("/");
+  File currentFile;
+
+  Serial.println(currentFile.name());
+  while (true) {
+    currentFile = root.openNextFile();
+    String fileName = currentFile.name();
+    Serial.println(fileName);
+    fileName.toUpperCase();
+    if (! currentFile) {
+      break;
+    } else if (currentFile.isDirectory()) {
+      continue;
+    } else if(!fileName.equals("BOOT.WAV")) {
+      totalTracks++;
+    }
+    currentFile.close();
+  }
+
+
+  Serial.print(totalTracks);
+  Serial.println(" tracks found");
+
+
+  if (totalTracks < 0) {
+    Serial.println("Not enough tracks, cannot continue.");
+    cww_MorseTx morse(0, 10, 9, 1000);
+    morse.send("NO WAV");
+    while (true) {
+    }
+  }
 
   tmrpcm.speakerPin = 9;
   tmrpcm.setVolume(5);
 
   randomSeed(analogRead(A0));
-
-  SD.begin(SD_ChipSelectPin);
-  Serial.println("Audio start");
   tmrpcm.play("BOOT.WAV");
 }
 
